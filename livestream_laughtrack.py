2743c0d7046bbc9ba86299d9b3a7db7026530f02
While-Luke <lukeellis3141@gmail.com>
2024-10-30 20:16:33 +0800

    Added very basic laughtrack sound

--- livestream_laughtrack.py
+++ livestream_laughtrack.py
@@ -1,18 +1,22 @@
 import pytchat
-from datetime import datetime 
+import time
+import os
+from playsound import playsound
 
-chat = pytchat.create(video_id="")
+#https://www.soundfishing.eu/sound/group-laugh
+
+chat = pytchat.create(video_id="qdczJpv8RCc")
 print("Starting")
 
-laugh_emotes = ["lol"]
+laugh_emotes = ["草の字", "草"]
 
 num_chats = 0
 num_laughs = 0
 moving_average = 0
-rate_factor = 0.5
+rate_factor = 0.8
 
 while chat.is_alive():
-    start = datetime.now()
+    start = time.time()
 
     for c in chat.get().sync_items():
         if any(laugh in c.message for laugh in laugh_emotes):
@@ -20,10 +24,17 @@ while chat.is_alive():
         num_chats += 1
         #print(f"{c.datetime} [{c.author.name}]- {c.message}")
 
-    if (datetime.now() - start).total_seconds() > 1:
+    if time.time() - start > 0.5:
         moving_average = rate_factor*num_laughs/num_chats + (1-rate_factor)*moving_average
-        moving_average *= 0.9
+        moving_average *= 0.95
         print(moving_average)
+
+        if moving_average > 0.15:
+            try:
+                audio_file = os.path.dirname(__file__) + 'medium_laugh.mp3'
+                playsound(audio_file, block = False)
+            except:
+                print("Error playing sound")
 
         num_chats = 0
         num_laughs = 0
